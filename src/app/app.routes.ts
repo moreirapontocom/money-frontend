aca51b11c2a8cff5e287dc5b972cc814007d8140
Lucas Moreira <moreirapontocom@gmail.com>
2022-11-16 14:49:21 -0300

    Update Angular 13 to 14

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { DashboardComponent } from "./views/dashboard/dashboard.component";
+import { Routes as Routes_1 } from "@angular/router";
 
-export const Routes = [
+export const Routes: Routes_1 = [
     { path: '', component: DashboardComponent, pathMatch: 'full' },
 ];
